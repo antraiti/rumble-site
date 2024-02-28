441c070f2b6954f5121bc006a5b6c294b07dfdee
Anthony <tony.raiti@gmail.com>
2024-02-27 21:48:12 -0500

    fixing public page styling for mobile support on mobile

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -22,12 +22,32 @@ export default function NavBar() {
     return (
         <div className="navbar bg-base-100 text-base-content">
             <div className="navbar-start">
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn btn-ghost md:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </div>
+                    <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+                        <Link href="/rules" className="btn btn-ghost justify-start">Rules</Link>
+                        <Link href="/banlist"  className="btn btn-ghost justify-start">Banlist</Link>
+                        {!username &&
+                        <Link href="login" className="btn">Sign In</Link>
+                        }
+                        {username &&
+                        <div>
+                        <Link href="/events" className="btn btn-ghost">Events</Link>
+                        <Link href="/decks" className="btn btn-ghost">Decks</Link>
+                        <Link href="/stats" className="btn btn-ghost">Stats</Link> 
+                        <Link href="/profile" className="btn btn-ghost">{username}</Link>
+                        <div onClick={handleSignout} className="btn btn-ghost">Sign Out</div>
+                        </div>}
+                    </ul>
+                </div>
                 <Link href="/" className="btn btn-ghost text-xl">Rumble MTG</Link>
-                <Link href="/rules" className="btn btn-ghost">Rules</Link>
-                <Link href="/banlist"  className="btn btn-ghost">Banlist</Link>
+                <Link href="/rules" className="btn btn-ghost hidden md:flex">Rules</Link>
+                <Link href="/banlist"  className="btn btn-ghost hidden md:flex">Banlist</Link>
                 {/* <Link href="/rules" className="btn btn-ghost">Deck Check</Link> */}
             </div>
-            <div className="navbar-end">
+            <div className="navbar-end hidden md:flex">
                 {!username &&
                 <Link href="login" className="btn">Sign In</Link>
                 }
--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className="flex gap-5 mx-auto justify-center">
-          <div className="card w-72 bg-base-100 shadow-xl">
+      <div className="grid md:grid-cols-3 grid-cols-1 gap-5 mx-auto place-items-center max-w-5xl">
+          <div className="card w-72 h-72 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
               <img src="/images/blank-playing-cards.png" className="rounded-xl brightness-0 invert h-28" />
             </figure>
@@ -18,7 +18,7 @@ export default function Home() {
               <h2 className="card-title text-center self-center">60 Card Singleton Decks</h2>
             </div>
           </div>
-          <div className="card w-72 bg-base-100 shadow-xl">
+          <div className="card w-72 h-72 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
               <img src="/images/heart-outline.png" className="rounded-xl brightness-0 invert h-28" />
             </figure>
@@ -26,7 +26,7 @@ export default function Home() {
               <h2 className="card-title text-center self-center">20 Starting Life Total</h2>
             </div>
           </div>
-          <div className="card w-72 bg-base-100 shadow-xl">
+          <div className="card w-72 h-72 bg-base-100 shadow-xl">
             <figure className="px-10 pt-10">
               <img src="/images/dragon-icon.png" className="rounded-xl brightness-0 invert h-28" />
             </figure>
